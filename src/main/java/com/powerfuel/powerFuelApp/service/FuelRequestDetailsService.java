f3adb444d6da936d25609a8a90cbc558b753c562
Hasantha <hasantha.pathirana@hotmail.com>
2022-11-27 11:35:28 +0530

    fuel requests continuation

--- src/main/java/com/powerfuel/powerFuelApp/service/FuelRequestDetailsService.java
+++ src/main/java/com/powerfuel/powerFuelApp/service/FuelRequestDetailsService.java
@@ -1,4 +1,8 @@
 package com.powerfuel.powerFuelApp.service;
 
+import com.powerfuel.powerFuelApp.model.FuelRequestDetails;
+
 public interface FuelRequestDetailsService {
+
+    public FuelRequestDetails insertRequestDetails(FuelRequestDetails fuelrequestdetails);
 }
