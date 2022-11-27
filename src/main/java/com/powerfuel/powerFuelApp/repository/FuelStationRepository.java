8fb7ea8aadead0fb2bab90267544ab0e3fd49c90
Hasantha <hasantha.pathirana@hotmail.com>
2022-11-27 10:20:14 +0530

    Fuel Station queries updated

--- src/main/java/com/powerfuel/powerFuelApp/repository/FuelStationRepository.java
+++ src/main/java/com/powerfuel/powerFuelApp/repository/FuelStationRepository.java
@@ -10,6 +10,11 @@ import java.util.List;
 import java.util.Objects;
 
 public interface FuelStationRepository extends JpaRepository<FuelStation,Integer> {
+    /*-----Validate Station Registration----*/
+    @Query(value = "SELECT COUNT(*) FROM fuel_station WHERE email=?1 ",nativeQuery = true)
+    int validateStation(String email);
+
+
     /*---View Station Details-----*/
     @Query(value="SELECT * FROM fuel_station WHERE id=?1",nativeQuery = true)
     List<FuelStation> getStationDetails(int station_id);
@@ -17,8 +22,8 @@ public interface FuelStationRepository extends JpaRepository<FuelStation,Integer
     /*------Update Fuel Station table------*/
     @Transactional
     @Modifying
-    @Query(value="UPDATE fuel_station SET name=?1,mobile=?2,email=?3,diesel_capacity=?4,petrol_capacity=?5 WHERE id like ?6",nativeQuery = true)
-    void updateStationData(String name,String mobile,String email,double diesel,double petrol,int id);
+    @Query(value="UPDATE fuel_station SET name=?1,mobile=?2,diesel_capacity=?3,petrol_capacity=?4,status=?5 WHERE email like ?6",nativeQuery = true)
+    void updateStationData(String name,String mobile,double diesel,double petrol,String status,String email);
 
     /*---------Close Station-----*/
     @Transactional
--- src/main/java/com/powerfuel/powerFuelApp/service/FuelStationImpl.java
+++ src/main/java/com/powerfuel/powerFuelApp/service/FuelStationImpl.java
@@ -5,13 +5,38 @@ import com.powerfuel.powerFuelApp.repository.FuelStationRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Objects;
+
 @Service
 public class FuelStationImpl implements FuelStationService {
     @Autowired
-    private FuelStationRepository repositoory;
+    private FuelStationRepository repository;
 
     public FuelStation registerFuelStation(FuelStation fuelstation){
-        return repositoory.save(fuelstation);
+        return repository.save(fuelstation);
     }
+
+    public List<FuelStation> getStationDetails(int station_id){
+        return repository.getStationDetails(station_id);
+    }
+
+    public void updateStationData(String name,String mobile,double diesel,double petrol,String status,String email){
+        repository.updateStationData(name, mobile, diesel, petrol,status,email);
+    }
+
+    public void closeStation(String status,int id){
+        repository.closeStation(status, id);
+    }
+
+    public List<Objects> getCurrentCapacity(int id){
+        return repository.getCurrentCapacity(id);
+    }
+
+    public int validateStation(String email){
+        return repository.validateStation(email);
+    }
+
+
 
 }
