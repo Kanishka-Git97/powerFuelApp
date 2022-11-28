c1cc5c935b745595e9968e3d73e25a752085bccc
Purna Kanishka <purnakanishka97@icloud.com>
2022-11-28 09:26:36 +0530

    bugfixes

--- src/main/java/com/powerfuel/powerFuelApp/service/CustomerImpl.java
+++ src/main/java/com/powerfuel/powerFuelApp/service/CustomerImpl.java
@@ -5,11 +5,16 @@ import com.powerfuel.powerFuelApp.repository.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class CustomerImpl implements CustomerService{
     @Autowired
     private CustomerRepository repository;
     public Customer saveCustomer(Customer customer){
         return repository.save(customer);
+    }
+    public List<Customer> getCustomers(){
+        return repository.findAll();
     }
 }
