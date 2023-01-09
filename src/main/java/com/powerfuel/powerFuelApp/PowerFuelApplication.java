3b88be7a948691d5e23c1dfb4c84934d01072a43
Purna Kanishka <purnakanishka97@icloud.com>
2023-01-09 20:04:50 +0530

    refactoring

--- src/main/java/com/powerfuel/powerFuelApp/PowerFuelApplication.java
+++ src/main/java/com/powerfuel/powerFuelApp/PowerFuelApplication.java
@@ -2,12 +2,29 @@ package com.powerfuel.powerFuelApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.Scheduled;
+
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
 
 @SpringBootApplication
 public class PowerFuelApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(PowerFuelApplication.class, args);
+		while (true) {
+			Date date = new Date();
+			//Implement Fuel Refilling Methods and Token Expired Function
+			
+			try {
+				TimeUnit.SECONDS.sleep(3);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
 	}
 
 }
+
+
